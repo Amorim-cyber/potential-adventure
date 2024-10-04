e27344772caecb660faedb6625e9590ac17c72e6
lucasa <amorim22lucas@gmail.com>
2024-10-04 12:13:33 -0300

    Keep SECRET_KEY secure

--- potential_adventure/potential_adventure/settings.py
+++ potential_adventure/potential_adventure/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ra-)el6^vvle=kmb7e4ow0y0_b5s!n1vxbdfnp(!18qac(*98)'
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
